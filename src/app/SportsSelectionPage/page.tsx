681337ff3fbd542a8afde30e5570a6b60dd8ddc7
Yokie99 <kyleym@uci.edu>
2024-04-05 11:19:32 -0700

    added images and sports page

--- src/app/SportsSelectionPage/page.tsx
+++ src/app/SportsSelectionPage/page.tsx
@@ -1,43 +1,38 @@
 'use client'
 
 import React from 'react'
+import NavbarComponent from '../Components/NavbarComponent';
 
-const SportsPage = () => {
+const SportsSelectionPage = () => {
     const sports = [
         "Tennis",
         "American Football",
         "Basketball",
         "Soccer",
         "Baseball",
-        "Softball",
-        "Track and Field",
         "Volleyball",
         "Wrestling",
-        "Cross Country",
         "Golf",
-        "Swimming and Diving",
+        "Swimming",
         "Lacrosse",
-        "Cheerleading",
-        "Field Hockey",
-        "Ice Hockey",
-        "Rugby",
+        "Hockey",
         "Gymnastics",
+        "Track and Field",
         "Water Polo",
-        "Ultimate Frisbee"
       ];
     
       const SportsList = ({ sports:[] }) => {
         return (
             sports.map((sport, index) => (
               <div key={index} className=" flex justify-center py-3">
-                <button className=" bg-blue-300 h-36 w-[80%] rounded-lg text-5xl font-bold">{sport}</button>
+                <button className=" bg-blue-300 h-36 w-[90%] rounded-lg text-5xl font-bold">{sport}</button>
               </div>
             ))
         );
       };
   return (
     <>
-    
+    <NavbarComponent/>
       <h1 className="text-center text-4xl font-bold py-4">Select a Sport</h1>
       <div className="border-2 border-red-600 mx-7">
         <div className="grid grid-cols-2">
@@ -50,4 +45,4 @@ const SportsPage = () => {
   )
 }
 
-export default SportsPage
+export default SportsSelectionPage
