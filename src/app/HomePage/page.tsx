b9ae3f2abe15d8499b9cc511f8a6acc3a2c20caa
Jeriah Marson <jeriahmarson@gmail.com>
2024-04-08 15:49:10 -0700

    font

--- src/app/HomePage/page.tsx
+++ src/app/HomePage/page.tsx
@@ -14,9 +14,9 @@ const HomePage = () => {
 
 
 
-      <div className="grid grid-cols-5 mx-5">
+      <div className="grid grid-cols-5 mx-5 bg-gradient-to-b from-lime-200 from-10% via-lime-100 via-70% to-white to-100%">
           <div className="col-span-2 bg-orange-300  w-full py-8">
-            <p className="text-center">CALENDER WILL GO IN HERE</p>
+            <p className="text-center inter">CALENDER WILL GO IN HERE</p>
             <div className="flex justify-center">
               <div className=" bg-lime-300 w-[90%] h-[45vh]"></div>
             </div>
@@ -26,12 +26,12 @@ const HomePage = () => {
 
         <div className=" col-span-3 px-16">
           <div className="py-8">
-            <h1 className="text-center text-3xl font-bold">THIS IS TODAY'S DATE</h1>
+            <h1 className="text-center text-3xl font-titillium font-bold">THIS IS TODAY'S DATE</h1>
             <div className="">
               <ul style={{ listStyleType: 'square' }}>
-                <li className="my-3">EVENT 1</li>
-                <li className="my-3">EVENT 2</li>
-                <li className="my-3">EVENT 3 </li>
+                <li className="my-3 font-titillium">EVENT 1</li>
+                <li className="my-3 font-titillium">EVENT 2</li>
+                <li className="my-3 font-titillium">EVENT 3 </li>
               </ul>
             </div>
 
@@ -41,7 +41,7 @@ const HomePage = () => {
 
       </div>
 
-      <h1 className="text-center text-3xl font-bold py-4">UPCOMING EVENTS</h1>
+      <h1 className="text-center text-3xl font-titillium font-bold py-4">UPCOMING EVENTS</h1>
       <div className="border-2 border-red-600 h-[40vh] mx-5"></div>
     </>
   )
--- src/app/ProfilePage/page.tsx
+++ src/app/ProfilePage/page.tsx
@@ -13,7 +13,7 @@ const page = () => {
     <NavbarComponent/>
 
 
-    <div className="grid grid-cols-6 mx-7 py-8">
+    <div className="grid grid-cols-6 mx-7 py-8 bg-gradient-to-b from-lime-200 from-10% via-lime-100 via-70% to-white to-100%">
         <div className="col-span-2 bg-orange-100 w-full">
 
             <div className="flex justify-center">
@@ -27,25 +27,25 @@ const page = () => {
                 <div className="">
                     <ul style={{ listStyleType: "none" }}>
                         <li className="my-3">
-                            <div className='flex flex-row text-2xl'>
+                            <div className='flex flex-row text-2xl font-titillium'>
                                 <p className='pe-3 font-bold'>Name:</p>
                                 <p>Kyle Yok Eyh Ma</p>
                             </div>
                         </li>
                         <li className="my-3">
-                            <div className='flex flex-row text-2xl'>
+                            <div className='flex flex-row text-2xl font-titillium'>
                                 <p className='pe-3 font-bold'>Birthday:</p>
                                 <p>03/39</p>
                             </div>
                         </li>
                         <li className="my-3">
-                            <div className='flex flex-row text-2xl'>
+                            <div className='flex flex-row text-2xl font-titillium'>
                                 <p className='pe-3 font-bold'>Status:</p>
                                 <p>Coach, Admin</p>
                             </div>
                         </li>
                         <li className="my-3">
-                            <div className='flex flex-row text-2xl'>
+                            <div className='flex flex-row text-2xl font-titillium'>
                                 <p className='pe-3 font-bold'>Fun Fact:</p>
                                 <p>Born to nap forced to code</p>
                             </div>
@@ -54,13 +54,13 @@ const page = () => {
                     </ul>
                 </div>
                 <div className='mt-auto flex justify-center'>
-                    <button className='border-2 border-black rounded-lg min-w-36 h-14'> EDIT </button>
+                    <button className='border-2 border-black rounded-lg min-w-36 h-14 font-titillium'> EDIT </button>
                 </div>
         </div>
 
     </div>
 
-    <h1 className="text-center text-3xl font-bold py-4">Affiliated Programs</h1>
+    <h1 className="text-center text-3xl font-bold py-4 font-titillium">Affiliated Programs</h1>
 <div className="border-2 border-red-600 h-[40vh] mx-7"></div>
 </>
   )
