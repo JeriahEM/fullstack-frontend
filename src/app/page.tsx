5b4e164e6452914a61cc974dbfcb0d5c3c7a8dc0
Yokie99 <kyleym@uci.edu>
2024-04-09 15:51:40 -0700

    eod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
       // console.log(userData)
       // createAccount(userData);
       handleNewUserBool();
-      
+
     } else {
       //
 
@@ -69,50 +69,96 @@ export default function Home() {
   return (
     <div className="min-h-screen w-full Tennis ">
 
-      <div className="CenterScreen">
+      {newUserBool ?
 
-        <div className="text-center text-5xl text-white font-bebas tracking-[.15rem] pt-14 ">
-          <p>Court Monitor</p>
-        </div>
-        <div className="grid grid-flow-row justify-center">
-          <div className="min-w-96 p-8 rounded-lg">
-            <div className="items-center pb-8">
-              <h1 className="text-center text-4xl font-titillium text-white textShadow">{switchBool ? 'New User' : 'Login'}</h1>
-            </div>
-            <form className="flex max-w-md flex-col gap-4">
-              <div className="flex flex-row gap-3">
-                <div className="mb-2 block pt-2 text-3xl font-titillium text-white textShadow">
-                  <p>UserName:</p>
-                </div>
-                <input id="username" type="text" className="inputbg border-transparent" required onChange={(e) => setUsername(e.target.value)} />
+        <div className="CenterScreen">
+
+          <div className="text-center text-5xl text-white font-bebas tracking-[.15rem] pt-14 ">
+            <p>Court Monitor NEW USER BOOL</p>
+          </div>
+          <div className="grid grid-flow-row justify-center">
+            <div className="min-w-96 p-8 rounded-lg">
+              <div className="items-center pb-8">
+                <h1 className="text-center text-4xl font-titillium text-white textShadow">Fill out your information</h1>
               </div>
-              <div className="flex flex-row gap-3">
-                <div className="mb-2 block">
+              <form className="flex max-w-md flex-col gap-4">
+                <div className="flex flex-row gap-3">
                   <div className="mb-2 block pt-2 text-3xl font-titillium text-white textShadow">
-                    <p>Password:</p>
+                    <p>Full Name:</p>
                   </div>
+                  <input id="username" type="text" className="inputbg border-transparent" required onChange={(e) => setUsername(e.target.value)} />
                 </div>
-                <input id="password1" type="password" className="inputbg border-transparent" required onChange={(e) => setPassword(e.target.value)} />
-              </div>
+                <div className="flex flex-row gap-3">
+                  <div className="mb-2 block">
+                    <div className="mb-2 block pt-2 text-3xl font-titillium text-white textShadow">
+                      <p>E-Mail:</p>
+                    </div>
+                  </div>
+                  <input id="password1" type="password" className="inputbg border-transparent" required onChange={(e) => setPassword(e.target.value)} />
+                </div>
 
-              {/* <Button onClick={handleSubmit} className="bg-white text-black font-titillium">
-                <p className="text-3xl">{switchBool ? 'Next' : 'Enter'}</p>
-              </Button> */}
-              <div className="flex items-center place-content-center gap-2">
-                <button className="text-3xl font-titillium text-white" onClick={handleSwtich}>
-                  <p className="textShadow">{switchBool ? 'Already have an Account?' : 'Sign up'}</p>
+                <Button onClick={() => alert("its works")} className="bg-white text-black font-titillium">
+                  <p className="text-3xl">Enter</p>
+                </Button>
 
-                </button>
-              </div>
-              <Button onClick={()=> router.push('/HomePage')}>Submit</Button>
-            </form>
+                <Button onClick={() => router.push('/HomePage')}>Submit</Button>
+              </form>
+            </div>
+
           </div>
 
+
+        </div>
+        :
+
+        <div className="CenterScreen">
+
+          <div className="text-center text-5xl text-white font-bebas tracking-[.15rem] pt-14 ">
+            <p>Court Monitor</p>
+          </div>
+          <div className="grid grid-flow-row justify-center">
+            <div className="min-w-96 p-8 rounded-lg">
+              <div className="items-center pb-8">
+                <h1 className="text-center text-4xl font-titillium text-white textShadow">{switchBool ? 'New User' : 'Login'}</h1>
+              </div>
+              <form className="flex max-w-md flex-col gap-4">
+                <div className="flex flex-row gap-3">
+                  <div className="mb-2 block pt-2 text-3xl font-titillium text-white textShadow">
+                    <p>UserName:</p>
+                  </div>
+                  <input id="username" type="text" className="inputbg border-transparent" required onChange={(e) => setUsername(e.target.value)} />
+                </div>
+                <div className="flex flex-row gap-3">
+                  <div className="mb-2 block">
+                    <div className="mb-2 block pt-2 text-3xl font-titillium text-white textShadow">
+                      <p>Password:</p>
+                    </div>
+                  </div>
+                  <input id="password1" type="password" className="inputbg border-transparent" required onChange={(e) => setPassword(e.target.value)} />
+                </div>
+
+                <Button onClick={handleSubmit} className="bg-white text-black font-titillium">
+                  <p className="text-3xl">{switchBool ? 'Next' : 'Enter'}</p>
+                </Button>
+
+                <div className="flex items-center place-content-center gap-2">
+                  <button className="text-3xl font-titillium text-white" onClick={handleSwtich}>
+                    <p className="textShadow">{switchBool ? 'Already have an Account?' : 'Sign up'}</p>
+
+                  </button>
+                </div>
+                <Button onClick={() => router.push('/HomePage')}>Submit</Button>
+              </form>
+            </div>
+
+          </div>
+
+
         </div>
 
+      }
 
-      </div>
-      
+
 
     </div>
   );
