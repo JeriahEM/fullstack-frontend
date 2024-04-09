281f850aae2c997d4659f94a44f66c72095e5bd9
Jeriah Marson <jeriahmarson@gmail.com>
2024-04-09 15:57:57 -0700

    end of day

--- src/app/UserDirectoryPage/page.tsx
+++ src/app/UserDirectoryPage/page.tsx
@@ -4,8 +4,8 @@ import NavbarComponent from "../Components/NavbarComponent";
 import Image from "next/image";
 import image1 from "../assets/images/image1.png";
 import phUser from "../assets/images/Group13.png";
-import dummyUsers from "@/utils/DummyUser.json"
-
+import dummyUsers from "@/utils/DummyUser.json";
+import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined";
 
 // For this page we will need to call users id, pfp, username, status
 
@@ -16,17 +16,24 @@ const coaches = ["Kyle Yok Eyh Ma"];
 const user = ["Billy Willy"];
 
 const UserDirectoryPage = () => {
-
-
   const createUser = () => {
     return dummyUsers.map((user, idx) => (
-      <div key={idx} className="flex flex-row mt-6 text-2xl font-titillium items-center gap-x-9">
-            <Image src={phUser} alt={""} />
-            <p>{user.Username}</p>
-          </div>
+      <div
+        key={idx}
+        className="flex flex-row mt-6 text-2xl font-titillium items-center gap-x-9"
+      >
+        <div className="grow-0">
+          <Image src={phUser} alt={""} />
+        </div>
+        <div className="grow">
+          <p>{user.Username}</p>
+        </div>
+        <div className="grow-0">
+          <MoreVertOutlinedIcon />
+        </div>
+      </div>
     ));
   };
-
 
   return (
     <div className="bg-gradient-to-b from-lime-200 from-10% via-lime-100 via-70% to-white to-100% h-screen card w-full">
@@ -39,8 +46,15 @@ const UserDirectoryPage = () => {
           <p className="mt-4 text-3xl font-titillium">Program Owner</p>
           <hr className="h-px mt-2 bg-black border-0 dark:bg-gray-700" />
           <div className="flex flex-row mt-6 text-2xl font-titillium items-center gap-x-9">
-            <Image src={image1} alt={""} />
-            <p>Roger Federer</p>
+            <div className="grow-0">
+              <Image src={image1} alt={""} />
+            </div>
+            <div className="grow">
+              <p>Roger Federer</p>
+            </div>
+            <div className="grow-0 ">
+              <MoreVertOutlinedIcon />
+            </div>
           </div>
           <p className="mt-4 text-3xl font-titillium">Coaches</p>
           <hr className="h-px mt-2 bg-black border-0 dark:bg-gray-700" />
