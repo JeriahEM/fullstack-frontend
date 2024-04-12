12e24e15780367d59e1c4325cc12a4fd0e15ddb9
Yokie99 <kyleym@uci.edu>
2024-04-12 10:50:05 -0700

    disabled other button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -180,7 +180,7 @@ export default function Home() {
 
                 </button>
               </div>
-              <Button onClick={() => router.push('/HomePage')}>Submit</Button>
+              {/* <Button onClick={() => router.push('/HomePage')}>Submit</Button> */}
             </form>
           </div>
 
