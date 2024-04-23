98e8bdbe9b9727cf4964773a8ddeb1e438a563cb
Yokie99 <kyleym@uci.edu>
2024-04-23 14:36:58 -0700

    added calendar

---
+++ src/app/Components/AddEventModal.tsx
@@ -0,0 +1,111 @@
+"use client"
+import { Fragment, useEffect, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { CheckIcon, ExclamationTriangleIcon } from '@heroicons/react/20/solid'
+
+import TimeSelector from './TimeSelector'
+
+interface Event {
+    title: string;
+    start: Date | string;
+    end: Date | string;
+    allDay: boolean;
+    id: number;
+  }
+interface AddEventModalProps{
+        showModal: boolean
+        setShowModal: React.Dispatch<React.SetStateAction<boolean>>
+        handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void
+        handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+        newEvent: Event
+        handleCloseModal: () => void
+    }
+const AddEventModal = (props:AddEventModalProps) => {
+    
+    const handleTimeChange = (time: string) => {
+        console.log(`Selected time: ${time}`);
+        // You can use this time value for your event planning logic
+      };
+    
+    
+  return (
+    <>
+    <Transition.Root show={props.showModal} as={Fragment}>
+          <Dialog as="div" className="relative z-10" onClose={props.setShowModal}>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 z-10 overflow-y-auto">
+              <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                  enterTo="opacity-100 translate-y-0 sm:scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                  leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                >
+                  <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                    <div>
+                     
+                      <div className="mt-3 text-center sm:mt-5">
+                        <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                          Add Event
+                        </Dialog.Title>
+
+                        <form action="submit" onSubmit={props.handleSubmit}>
+                          <div className="mt-2">
+                            <input type="text" name="title" className="block w-full rounded-md border-0 py-1.5 text-gray-900 
+                            shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
+                            focus:ring-2 
+                            focus:ring-inset focus:ring-violet-600 
+                            sm:text-sm sm:leading-6"
+                              value={props.newEvent.title} onChange={(e) => props.handleChange(e)} placeholder="Title" />
+                          </div>
+
+                          <div className='mt-2 flex items-center gap-5'>
+                            <p>End Time:</p>
+                            <TimeSelector onChange={handleTimeChange}/>
+                          </div>
+                          
+                          <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                            <button
+                              type="submit"
+                              className="inline-flex w-full justify-center rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600 sm:col-start-2 disabled:opacity-25"
+                              disabled={props.newEvent.title === ''}
+                            >
+                              Create
+                            </button>
+                            <button
+                              type="button"
+                              className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                              onClick={props.handleCloseModal}
+
+                            >
+                              Cancel
+                            </button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </Dialog>
+        </Transition.Root>
+        </>
+  )
+}
+
+export default AddEventModal
--- src/app/HomePage/page.tsx
+++ src/app/HomePage/page.tsx
@@ -1,11 +1,122 @@
 'use client'
 
 import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
+import React, {Fragment, useEffect, useState } from 'react'
 import NavbarComponent from '../Components/NavbarComponent';
 
 
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin, { Draggable, DropArg } from '@fullcalendar/interaction'
+import timeGridPlugin from '@fullcalendar/timegrid'
+
+import { Dialog, Transition } from '@headlessui/react'
+import { CheckIcon, ExclamationTriangleIcon } from '@heroicons/react/20/solid'
+import { EventSourceInput } from '@fullcalendar/core/index.js'
+
+import AddEventModal from '../Components/AddEventModal';
+import DeleteEventModal from '../Components/DeleteEventModal';
+
 const HomePage = () => {
+  interface Event {
+    title: string;
+    start: Date | string;
+    end: Date | string;
+    allDay: boolean;
+    id: number;
+    //color
+    //program
+    //
+  }
+  const [events, setEvents] = useState([
+    { title: 'event 1', id: '1' },
+    { title: 'event 2', id: '2' },
+    { title: 'event 3', id: '3' },
+    { title: 'event 4', id: '4' },
+    { title: 'event 5', id: '5' },
+  ])
+
+  const [allEvents, setAllEvents] = useState<Event[]>([])
+  const [showModal, setShowModal] = useState(false)
+  const [showDeleteModal, setShowDeleteModal] = useState(false)
+  const [idToDelete, setIdToDelete] = useState<number | null>(null)
+  const [newEvent, setNewEvent] = useState<Event>({
+    title: '',
+    start: '',
+    end: '',
+    allDay: false,
+    id: 0
+  })
+
+  useEffect(() => {
+    let draggableEl = document.getElementById('draggable-el')
+    if (draggableEl) {
+      new Draggable(draggableEl, {
+        itemSelector: ".fc-event",
+        eventData: function (eventEl) {
+          let title = eventEl.getAttribute("title")
+          let id = eventEl.getAttribute("data")
+          let start = eventEl.getAttribute("start")
+          return { title, id, start }
+        }
+      })
+    }
+  }, [])
+
+  function handleDateClick(arg: { date: Date, allDay: boolean }) {
+    setNewEvent({ ...newEvent, start: arg.date, allDay: arg.allDay, id: new Date().getTime() })
+    setShowModal(true)
+  }
+  function addEvent(data: DropArg) {
+    const event = { ...newEvent, start: data.date.toISOString(), title: data.draggedEl.innerText, allDay: data.allDay, id: new Date().getTime() }
+    setAllEvents([...allEvents, event])
+  }
+
+  function handleDeleteModal(data: { event: { id: string } }) {
+    setShowDeleteModal(true)
+    setIdToDelete(Number(data.event.id))
+  }
+
+  function handleDelete() {
+    setAllEvents(allEvents.filter(event => Number(event.id) !== Number(idToDelete)))
+    setShowDeleteModal(false)
+    setIdToDelete(null)
+  }
+
+  function handleCloseModal() {
+    setShowModal(false)
+    setNewEvent({
+      title: '',
+      start: '',
+      end: '',
+      allDay: false,
+      id: 0
+    })
+    setShowDeleteModal(false)
+    setIdToDelete(null)
+  }
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setNewEvent({
+      ...newEvent,
+      title: e.target.value
+    })
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    setAllEvents([...allEvents, newEvent])
+    setShowModal(false)
+    console.log(newEvent)
+    setNewEvent({
+      title: '',
+      start: '',
+      end: '',
+      allDay: false,
+      id: 0
+    })
+  }
+
   
 
     const router = useRouter();
@@ -15,22 +126,51 @@ const HomePage = () => {
 
       <div className=" grid grid-cols-5 mx-7">
           <div className="col-span-2 bg-orange-300  w-full py-8 ">
-            <p className="text-center inter">CALENDER WILL GO IN HERE</p>
-            <div className="flex justify-center">
-              <div className=" bg-lime-300 w-[90%] h-[45vh]">
-                <div className='p-8 text-xl'>
-                <ul style={{ listStyleType: 'square' }}>
-                <li><p>Welcome to our Beta test: Currently all the navigation features in the navbar will take you somewhere.</p></li>
-                <li><p>Editing your profile will work, Images to be added</p></li>
-                <li><p>User Directory is currently filled with Dummy Data</p></li>
-                <li><p>Sports Selection screen is clickable as well</p></li>
-              </ul>
-                  
-                
-                </div>
-                
-              </div>
-            </div>
+            
+          <main className="p-3 h-full min-w-[300px] ">
+        
+            <FullCalendar
+              plugins={[
+                dayGridPlugin,
+                interactionPlugin,
+                timeGridPlugin
+              ]}
+              headerToolbar={{
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek'
+              }}
+              events={allEvents as EventSourceInput}
+              nowIndicator={true}
+              editable={true}
+              droppable={true}
+              selectable={true}
+              selectMirror={true}
+              dateClick={handleDateClick}
+              drop={(data) => addEvent(data)}
+              eventClick={(data) => handleDeleteModal(data)}
+            />
+          
+
+        <AddEventModal 
+          showModal={showModal}
+          setShowModal={setShowModal}
+          handleSubmit={handleSubmit}
+          handleChange={handleTitleChange}
+          newEvent={newEvent}
+          handleCloseModal={handleCloseModal}
+        />
+        <DeleteEventModal
+         showDeleteModal={showDeleteModal}
+         setShowDeleteModal={setShowDeleteModal}
+         handleDelete={handleDelete}
+         handleCloseModal={handleCloseModal}
+        />
+
+
+        
+        
+      </main>
             
           </div>
         
