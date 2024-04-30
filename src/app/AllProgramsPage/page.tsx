fff9012c3ab7b2082977aea8afbfd35efcead852
Jeriah Marson <jeriahmarson@gmail.com>
2024-04-30 15:54:47 -0700

    started modal for program

--- src/app/AllProgramsPage/page.tsx
+++ src/app/AllProgramsPage/page.tsx
@@ -1,15 +1,33 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import NavbarComponent from '../Components/NavbarComponent'
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import { Button } from 'flowbite-react';
+
+// const [openModal, setOpenModal] = useState(false);
+// const [openConfirmModal, setOpenConfirmModal] = useState(false);
+// const [openCancelModal, setOpenCancelModal] = useState(false);
+
+// // const handleCloseModals = () => {
+// //   setOpenModal(false);
+// //   setOpenConfirmModal(false);
+// //   setOpenCancelModal(false);
+// // }
+// // const handleNewProgram = () => {
+// //   setOpenModal(false);
+// //   setOpenConfirmModal(false);
+// //   setOpenCancelModal(false);
+// // }
+
 
 const AllProgramsPage = () => {
   return (
     <div className="bg-gradient-to-b from-lime-200 from-10% via-lime-100 via-70% to-white to-100% h-screen card w-full">
       <NavbarComponent />
-      <div className="flex flex-col items-center text-3xl font-titillium font-bold pt-14">
+      <div className="flex flex-col  text-3xl font-titillium font-bold pt-14">
         <p>All Programs</p>
+        <Button className=''>Create new program</Button>
       </div>
       <div className="grid grid-cols-1 mx-7">
           <hr className="h-px mt-2 bg-black border-0 dark:bg-gray-700" />
